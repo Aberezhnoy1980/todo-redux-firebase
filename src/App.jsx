52b9000ee56a1824fc0fbcabb9019f64aefbfc0c
Aleksandr <Berezhnoy_aa@mail.ru>
2025-12-04 16:27:41 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Provider } from 'react-redux';
+import store from './app/store';
+import TodoApp from './components/TodoApp';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <TodoApp />
+    </Provider>
+  );
+}
